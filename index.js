edadb5bb01831a756418d8b56a85c9357a381d5a
sinan-894 <mohammedsinan1011@gmail.com>
2024-08-11 19:22:32 +0530

    Add classes to the rows and individual squares also borders for testing

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const containerDiv = document.querySelector('.container');
 
 for(let i=0;i<16;i++){
     const divRow = document.createElement('div');
+    divRow.classList.add('row')
     for(let j=0;j<16;j++){
         const divSquare = document.createElement('div');
+        divSquare.classList.add('square')
         divRow.appendChild(divSquare);
 
     }
