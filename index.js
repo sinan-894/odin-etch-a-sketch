d1c64ee2aab9b2554e855672164d426ed2fefe0f
sinan-894 <mohammedsinan1011@gmail.com>
2024-08-11 21:03:06 +0530

    Add functionalities to the button to allow to create custom sized sketchbox

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
 
 
+const divMain = document.querySelector('.main');
+
+createSketchBox(16);
+function getNumberOfSquares(){
+    const numberOfSquares = parseInt(prompt("Enter the number of squares needed in each side(3-100)"));
+    const divContainer = document.querySelector(".container");
+    divMain.removeChild(divContainer);
+    createSketchBox(numberOfSquares)
 
 
 
+}
 
-const containerDiv = document.querySelector('.container');
-
-
-for(let i=0;i<16;i++){
-    const divRow = document.createElement('div');
-    divRow.classList.add('row')
-    for(let j=0;j<16;j++){
-        const divSquare = document.createElement('div');
-        divSquare.classList.add('square')
-        // divSquare.addEventListener('mouseover',()=>{
-        //     divSquare.classList.add('on-hover');
-        // })
-        divRow.appendChild(divSquare);
-
+function createSketchBox(numberOfSquares){
+    const divContainer = document.createElement('div');
+    divContainer.classList.add("container");
+    divMain.appendChild(divContainer)
+    
+    for(let i=0;i<numberOfSquares;i++){
+        const divRow = document.createElement('div');
+        divRow.classList.add('row')
+        for(let j=0;j<numberOfSquares;j++){
+            const divSquare = document.createElement('div');
+            divSquare.classList.add('square')
+            divSquare.addEventListener('mouseover',()=>{
+                divSquare.classList.add('on-hover');
+            })
+            divRow.appendChild(divSquare);
+    
+        }
+        divContainer.appendChild(divRow)
+    
     }
-    containerDiv.appendChild(divRow)
-
 }
 
 
